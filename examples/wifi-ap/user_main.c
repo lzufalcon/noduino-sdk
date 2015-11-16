2d20885257e83b84d2bcbd6e527baefa2ca1379e
Jack Tan <jiankemeng@gmail.com>
2015-11-16 21:58:08 +0800

    update the header of all the user_main.c os examples

--- examples/wifi-ap/user_main.c
+++ examples/wifi-ap/user_main.c
@@ -15,7 +15,6 @@
  *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
 */
-
 #include "osapi.h"
 #include "os_type.h"
 #include "user_interface.h"
--- examples/wps/user_main.c
+++ examples/wps/user_main.c
@@ -1,15 +1,20 @@
 /*
- * Copyright 2013-2014 Espressif Systems (Wuxi)
+ *  Copyright (c) 2015 - 2025 MaiKe Labs
  *
- * FileName: user_main.c
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
  *
- * Description: entry file of user application
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
  *
- * Modification history:
- *     2015/7/3, v1.0 create this file.
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
 */
-
 #include "osapi.h"
 #include "user_interface.h"
 
