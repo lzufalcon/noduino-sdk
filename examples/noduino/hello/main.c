997ef738c6119e3060a9306e237dd6f5180d0568
Jack Tan <jiankemeng@gmail.com>
2015-11-23 22:47:24 +0800

    add a hello world example of lib noduino

---
+++ examples/noduino/hello/main.c
@@ -0,0 +1,35 @@
+/*
+ *  Copyright (c) 2015 - 2025 MaiKe Labs
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+*/
+#include "noduino.h"
+
+void setup()
+{
+	uart_init(115200, 115200);
+
+	pinMode(2, OUTPUT);
+	digitalWrite(2, LOW);
+}
+
+void loop()
+{
+	digitalWrite(2, HIGH);
+	delay(1000);
+	digitalWrite(2, LOW);
+	delay(1000);
+	uart0_sendStr("Hello world!\n");
+}
