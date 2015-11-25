ecd90a9bda14ce9801314e6f749fdf8e2a766ca9
Jack Tan <jiankemeng@gmail.com>
2015-11-25 21:08:10 +0800

    update the hello world example of libnoduino
    make it simple and beautiful

--- examples/noduino/hello/main.c
+++ examples/noduino/hello/main.c
@@ -19,17 +19,11 @@
 
 void setup()
 {
-	uart_init(115200, 115200);
-
-	pinMode(2, OUTPUT);
-	digitalWrite(2, LOW);
+	serial_begin(115200);
 }
 
 void loop()
 {
-	digitalWrite(2, HIGH);
-	delay(1000);
-	digitalWrite(2, LOW);
-	delay(1000);
-	uart0_sendStr("Hello world!\n");
+	serial_print("Hello World!\n");
+	delay(1500);
 }
